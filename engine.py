8f2f17a95cfa6ec8169065804c8c03c1229f4983
Kevin <kevin.jankowski0@gmail.com>
2025-04-26 16:28:36 +0200

    FIN scan method

--- engine.py
+++ engine.py
@@ -1,7 +1,4 @@
 import socket
-from asyncio import timeout
-from sys import flags
-
 from scapy.layers.inet import IP, TCP, sr1
 import time
 import ColoredPortStatus
@@ -16,6 +13,7 @@ def resolve_hostname(hostname):
     Returns:
         str: Resolved hostname in numeric format (fe. localhost -> 127.0.0.1)
     """
+
     try:
         ip = socket.gethostbyname(hostname)
         print(f"Start scanning host: {hostname} ({ip})")
@@ -32,7 +30,6 @@ def tcp_scan(target_host, ports):
         target_host (str): address of target host
         ports (list): list of ports for scan
     """
-
 
     # Translate a host name to IPv4 address format if necessary
     ip = resolve_hostname(target_host)
@@ -69,17 +66,14 @@ def tcp_scan(target_host, ports):
             print(f"- {port}: error - {e}")
 
 
-
 def syn_scan(target_host, ports):
     """
     Scan ports of target host with not full handshake method (SYN). Print results of scan in CLI.
 
     Args:
-        target_host (str): IP address of target host
+        target_host (str): address of target host
         ports (list): list of ports for scan
     """
-
-    open_ports = {}
 
     # Translate a host name to IPv4 address format if necessary
     ip = resolve_hostname(target_host)
@@ -131,7 +125,7 @@ def udp_scan(target_host, ports):
     Scan ports of target host with UDP method. Print results of scan in CLI.
 
     Args:
-        target_host (str): IP address of target host
+        target_host (str): address of target host
         ports (list): list of ports for scan
     """
 
@@ -169,3 +163,38 @@ def udp_scan(target_host, ports):
 
         except Exception as e:
             print(f"Error has occurred - {e}")
+
+
+def fin_scan(target_host, ports):
+    """
+    Scan ports of target host with FIN method. Print results of scan in CLI.
+
+    Args:
+        target_host (str): address of target host
+        ports (list): list of ports for scan
+    """
+
+    # Translate a host name to IPv4 address format if necessary
+    ip = resolve_hostname(target_host)
+
+    for port in ports:
+        # Build TCP packet with SYN flag
+        ip_layer = IP(dst=ip)
+        tcp_layer = TCP(dport=port, flags="F")
+        packet = ip_layer / tcp_layer
+
+        # Send packet and return for response
+        start_time = time.time()
+        response = sr1(packet, timeout=1, verbose=False)
+        end_time = time.time()
+
+        # Calculate response time in ms
+        response_time = round((end_time - start_time) * 1000, 2)
+
+        # Results interpretation
+        if response is None:
+            print(f"- {port}: {ColoredPortStatus.opened()} (response time: {response_time} ms)")
+
+        elif response[TCP].flags == 0x14:
+            print(f'- {port}: {ColoredPortStatus.closed()}')
+
