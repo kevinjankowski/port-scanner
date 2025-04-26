f98ca3fe333092f5d7b63534210073aa2e780dfc
Kevin <kevin.jankowski0@gmail.com>
2025-04-26 19:00:47 +0200

    changed udp method

--- engine.py
+++ engine.py
@@ -1,5 +1,5 @@
 import socket
-from scapy.layers.inet import IP, TCP, sr1
+from scapy.layers.inet import IP, TCP, sr1, UDP, ICMP
 import time
 import ColoredPortStatus
 
@@ -120,7 +120,7 @@ def syn_scan(target_host, ports):
             print(f"Error has occurred - {e}")
 
 
-def udp_scan(target_host, ports):
+def udp_scan2(target_host, ports):
     """
     Scan ports of target host with UDP method. Print results of scan in CLI.
 
@@ -135,19 +135,20 @@ def udp_scan(target_host, ports):
     for port in ports:
         # Create UDP socket (AF_INTER - address family IPv4, SOCK_DGRAM - connection type UDP
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.settimeout(1)  # Timeout set for 1 second
+        s.settimeout(10)  # Timeout set for 1 second
 
         try:
             # send empty datagram
             start_time = time.time()
             s.sendto(b'', (ip, port))
+
+            # try to receive a response
+            data, addr = s.recvfrom(1024)
             end_time = time.time()
 
             # Calculate response time in ms
             response_time = round((end_time - start_time) * 1000, 2)
 
-            # try to receive a response
-            data, addr = s.recvfrom(1024)
             print(f"- {port}: {ColoredPortStatus.opened()} (response time: {response_time} ms)")
 
             # Close socket
@@ -160,6 +161,58 @@ def udp_scan(target_host, ports):
         except socket.timeout:
             # Timeout means, that port is probably filtered
             print(f"- {port}: {ColoredPortStatus.opened()}|{ColoredPortStatus.filtered()}")
+
+        except Exception as e:
+            print(f"Error has occurred - {e}")
+
+def udp_scan(target_host, ports):
+    """
+    Scan ports of target host with UDP method. Print results of scan in CLI.
+
+    Args:
+        target_host (str): address of target host
+        ports (list): list of ports for scan
+    """
+
+    # Translate a host name to IPv4 address format if necessary
+    ip = resolve_hostname(target_host)
+
+    for port in ports:
+
+        try:
+            # Build UDP packet
+            ip_layer = IP(dst=ip)
+            udp_layer = UDP(dport=port)
+            packet = ip_layer / udp_layer
+
+            # Send packet and return for response
+            start_time = time.time()
+            response = sr1(packet, timeout=1, verbose=False)
+            end_time = time.time()
+
+            # Calculate response time in ms
+            response_time = round((end_time - start_time) * 1000, 2)
+
+            # Result interpretation
+            if response is None:
+                print(f"- {port}: {ColoredPortStatus.opened()}|{ColoredPortStatus.filtered()}")
+
+            elif response.haslayer(UDP):
+                print(f"- {port}: {ColoredPortStatus.opened()} (response time: {response_time} ms)")
+
+            elif response.haslayer(ICMP):
+                icmp_code = response.getlayer(ICMP).code
+                icmp_type = response.getlayer(ICMP).type
+
+
+                # if icmp type=3 and code=3 it means, that we received ICMP Host unreachable = closed
+                if icmp_code == 3 and icmp_type == 3:
+                    print(f"- {port}: {ColoredPortStatus.closed()}")
+                else:
+                    print(f"- {port}: {ColoredPortStatus.filtered()}")
+
+        except PermissionError:
+            print("Operation not permitted!")
 
         except Exception as e:
             print(f"Error has occurred - {e}")
@@ -199,7 +252,9 @@ def fin_scan(target_host, ports):
 
             elif response[TCP].flags == 0x14:
                 print(f'- {port}: {ColoredPortStatus.closed()}')
+
         except PermissionError:
             print("Operation not permitted!")
 
-
+        except Exception as e:
+            print(f"Error has occurred - {e}")
